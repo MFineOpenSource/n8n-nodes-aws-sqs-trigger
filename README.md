0c37cd904fede49734c8fdebac79051b6a2f63c1
Prasenjit Ghoshal <prasenjit.ghoshal@mfine.co>
2022-08-14 18:07:37 +0530

    Initial Commit AWS SQS Trigger Node

---
+++ README.md
@@ -0,0 +1,22 @@
+![Banner image](https://user-images.githubusercontent.com/10284570/173569848-c624317f-42b1-45a6-ab09-f0ea3c247648.png)
+
+# n8n-nodes-aws-sqs-trigger
+
+This repo contains AWS SQS Trigger Node for consuming Queue data. 
+Aws SQS node requires credential to communicate with AWS. This node uses existing AWS credential from n8n
+
+## Prerequisites
+
+You need the following installed on your development machine:
+
+* [git](https://git-scm.com/downloads)
+* Node.js and npm. Minimum version Node 16. You can find instructions on how to install both using nvm (Node Version Manager) for Linux, Mac, and WSL [here](https://github.com/nvm-sh/nvm). For Windows users, refer to Microsoft's guide to [Install NodeJS on Windows](https://docs.microsoft.com/en-us/windows/dev-environment/javascript/nodejs-on-windows).
+* Install n8n with:
+	```
+	npm install n8n -g
+	```
+* Recommended: follow n8n's guide to [set up your development environment](https://docs.n8n.io/integrations/creating-nodes/build/node-development-environment/).
+
+## License
+
+Apache 2.0
---
+++ nodes/AwsSqsTrigger/GenericFunctions.ts
@@ -0,0 +1,112 @@
+import { URL } from 'url';
+import { Request, sign } from 'aws4';
+import { OptionsWithUri } from 'request';
+import { parseString as parseXml } from 'xml2js';
+
+import {
+	IExecuteFunctions,
+	IHookFunctions,
+	ILoadOptionsFunctions,
+	ITriggerFunctions,
+	IWebhookFunctions,
+} from 'n8n-core';
+
+import { ICredentialDataDecryptedObject, NodeApiError, NodeOperationError } from 'n8n-workflow';
+
+function getEndpointForService(
+	service: string,
+	credentials: ICredentialDataDecryptedObject,
+): string {
+	let endpoint;
+	if (service === 'lambda' && credentials.lambdaEndpoint) {
+		endpoint = credentials.lambdaEndpoint;
+	} else if (service === 'sns' && credentials.snsEndpoint) {
+		endpoint = credentials.snsEndpoint;
+	} else if (service === 'sqs' && credentials.sqsEndpoint) {
+		endpoint = credentials.sqsEndpoint;
+	} else {
+		endpoint = `https://${service}.${credentials.region}.amazonaws.com`;
+	}
+	return (endpoint as string).replace('{region}', credentials.region as string);
+}
+
+export async function awsApiRequest(
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions | IWebhookFunctions | ITriggerFunctions,
+	service: string,
+	method: string,
+	path: string,
+	body?: string,
+	headers?: object,
+	// tslint:disable-next-line:no-any
+): Promise<any> {
+	const credentials = await this.getCredentials('aws');
+
+	// Concatenate path and instantiate URL object so it parses correctly query strings
+	const endpoint = new URL(getEndpointForService(service, credentials) + path);
+
+	// Sign AWS API request with the user credentials
+	const signOpts = { headers: headers || {}, host: endpoint.host, method, path, body } as Request;
+	const securityHeaders = {
+		accessKeyId: `${credentials.accessKeyId}`.trim(),
+		secretAccessKey: `${credentials.secretAccessKey}`.trim(),
+		sessionToken: credentials.temporaryCredentials
+			? `${credentials.sessionToken}`.trim()
+			: undefined,
+	};
+
+	sign(signOpts, securityHeaders);
+
+	const options: OptionsWithUri = {
+		headers: signOpts.headers,
+		method,
+		uri: endpoint.href,
+		body: signOpts.body,
+	};
+
+	try {
+		return await this.helpers.request!(options);
+	} catch (error) {
+		throw new NodeApiError(this.getNode(), error, { parseXml: true });
+	}
+}
+
+export async function awsApiRequestREST(
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions | ITriggerFunctions,
+	service: string,
+	method: string,
+	path: string,
+	body?: string,
+	headers?: object,
+	// tslint:disable-next-line:no-any
+): Promise<any> {
+	const response = await awsApiRequest.call(this, service, method, path, body, headers);
+	try {
+		return JSON.parse(response);
+	} catch (error) {
+		return response;
+	}
+}
+
+export async function awsApiRequestSOAP(
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions | IWebhookFunctions | ITriggerFunctions,
+	service: string,
+	method: string,
+	path: string,
+	body?: string,
+	headers?: object,
+	// tslint:disable-next-line:no-any
+): Promise<any> {
+	const response = await awsApiRequest.call(this, service, method, path, body, headers);
+	try {
+		return await new Promise((resolve, reject) => {
+			parseXml(response, { explicitArray: false }, (err, data) => {
+				if (err) {
+					return reject(err);
+				}
+				resolve(data);
+			});
+		});
+	} catch (error) {
+		return response;
+	}
+}
